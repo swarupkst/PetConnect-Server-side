c173951e363ce2885cdd6f24342914f032bdb7e8
Swarup <swarupkst@gmail.com>
2026-06-06 01:29:23 +0600

    initialize express server with mongodb, dotenv, and cors setup

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+
+run();
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
